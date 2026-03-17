4fc3917f8721d38bfd0700ad229618048bfd4836
leandra-67 <leandra.moscatiello@sbw-media.ch>
2026-03-17 13:54:01 +0100

    fix 2

---
+++ htdocs/index.php
@@ -0,0 +1,5 @@
+<?php
+// Root entry point - redirect into the "pages" folder.
+header('Location: pages/index.php');
+exit;
+?>
---
+++ htdocs/pages/index.php
@@ -0,0 +1,5 @@
+<?php
+// Redirect to the main application landing page
+header('Location: form.php');
+exit;
+?>
